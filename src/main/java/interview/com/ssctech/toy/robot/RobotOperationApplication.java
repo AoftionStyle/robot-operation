0372026bb87913a871524fdd50211643ca386143
AOFTION <chutipon.ppn@gmail.com>
2024-04-30 23:54:28 +0700

    Initial Project Robot

---
+++ src/main/java/interview/com/ssctech/toy/robot/RobotOperationApplication.java
@@ -0,0 +1,13 @@
+package interview.com.ssctech.toy.robot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotOperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobotOperationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/interview/com/ssctech/toy/robot/RobotOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package interview.com.ssctech.toy.robot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(properties = { "app.file.input=file" })
+class RobotOperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

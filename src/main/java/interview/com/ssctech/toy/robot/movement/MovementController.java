20a6381fab4cc37e5707c85d01c146d0ead714fb
AOFTION <chutipon.ppn@gmail.com>
2024-05-01 00:11:17 +0700

    Reading file from properties value

--- src/main/java/interview/com/ssctech/toy/robot/movement/MovementController.java
+++ src/main/java/interview/com/ssctech/toy/robot/movement/MovementController.java
@@ -1,25 +1,48 @@
 package interview.com.ssctech.toy.robot.movement;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Controller;
+
+import lombok.Getter;
 
 @Controller
 public class MovementController implements CommandLineRunner {
 
+  @Getter
+  @Value("${app.file.input}")
+  private String fileInput;
+
   @Override
   public void run(String... args) throws Exception {
     System.out.println("Movement CommandController");
-    //TODO
     // read file
-    // open loop read line
+    System.out.println("fileInput: " + fileInput);
+    Path filePath = new File(fileInput).toPath();
 
-    // if PLACE then place()
-    // initial movementService every PLACE
-    // if MOVE or LEFT or RIGHT then move() or left() or right()
-    // If REPORT then report()
-    // clear after report or might be not when new
+    // open-close file with try-resource
+    try (BufferedReader br = new BufferedReader(
+        new InputStreamReader(Files.newInputStream(filePath), StandardCharsets.UTF_8))) {
+      String line;
 
-    // close file
+      // open loop read line
+      MovementService movementService = null;
+      while ((line = br.readLine()) != null) {
+        System.out.println(line);
+        if ("PLACE".equals(line.split(" ")[0])){
+          movementService = new MovementService();
+        }
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
 }
